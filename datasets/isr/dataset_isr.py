29078086188ca65b94ad09a924f75a8e86ac4fed
OlineRanum <oline.a.ranum@gmail.com>
2024-01-17 11:59:26 +0100

    Cleaned data reader

--- datasets/isr/dataset_isr.py
+++ datasets/isr/dataset_isr.py
@@ -10,139 +10,148 @@ from torch_geometric.loader import DataLoader
 
 class ISRDataReader:
     def __init__(self, data_dir, args):
-        batch_size = args.batch_size
+        print('Reading data...')
+        
         self.args = args
-        self.file_path = os.path.join(data_dir, args.root_metadata)
-        self.pkl_folder = os.path.join(data_dir, args.root_poses)
-        self.batch_size = batch_size
-        self.n_nodes = 27
-        # Indexes for reduction of graph nodes
-        self.pose_indexes = [0, 2, 5, 11, 12, 13, 14, 33, 37, 38, 41, 42, 45, 46, 49, 50, 53, 54, 58, 59, 62, 63, 66, 67, 70, 71, 74]
+        self.N_NODES = args.n_nodes
+
+        file_path = os.path.join(data_dir, args.root_metadata)
+        pickle_path = os.path.join(data_dir, args.root_poses)
+        
+
+        # Load metadata
+        metadata = self._load_metadata(file_path)
+        self._load_gloss_pose_dict(metadata)
+        self.data_dict = self._load_pkl_files(pickle_path)
+        
+        # Treat each frame as individual data points
+        if args.temporal_configuration == 'per_frame':
+            self.data_dict = self._seperate_temporal_dimension(self.data_dict)
 
         
-        self.data_dict = self._load_and_split_data(temporal_configuration = args.temporal_configuration)
-
-
-    def _load_and_split_data(self, temporal_configuration):
-        metadata = self._load_metadata()
-        self.gloss_dict = self._load_gloss_pose_dict(metadata)
-        data_dict = self._load_pkl_files()
+        # Build spatio temporal graph 
+        elif args.temporal_configuration == 'spatio_temporal':
+            self.data_dict = self._build_spatio_temporal_graph(self.data_dict)
         
-        # Decouple time and look at each frame as a singular instance
-        if temporal_configuration == 'per_frame':
-            data_dict = self.seperate_temporal_dimension(data_dict)
-            data_dict = self.clean_padding(data_dict)
+        else:
+            raise ValueError('Temporal configuration not recognized')
         
-        elif temporal_configuration == 'spatio_temporal':
-            data_dict = self.build_spatio_temporal_graph(data_dict)
-            
 
-        return data_dict
-
-    ######################
-    # A. Reading functionalities
-    ######################
-
-    def _load_metadata(self):
-        with open(self.file_path, 'r') as file:
+    def _load_metadata(self, file_path):
+        """ Load the metadata from the json file
+        """
+        with open(file_path, 'r') as file:
             return json.load(file)
 
     def _load_gloss_pose_dict(self, metadata):
-        gloss_dict = {}
+        """ Load the gloss dictionary from the metadata
+        """
+        self.gloss_dict = {}
         for item in metadata:  
-            gloss = item['gloss']
-            gloss_dict.setdefault(gloss, []).extend([(instance['video_id'], instance['split']) for instance in item['instances']])
-        return gloss_dict
+            self.gloss_dict.setdefault(item['gloss'], []).extend(
+                [(instance['video_id'], instance['split']) for instance in item['instances']])
 
-    def _load_pkl_files(self):
-        data_dict = {}
-        labels = {word: index for index, word in enumerate(list(self.gloss_dict.keys()))}
-        for gloss, metadata in self.gloss_dict.items():
-            for vid_id, split in metadata:
-                pkl_path = os.path.join(self.pkl_folder, f'{vid_id}.pkl')
-                if os.path.exists(pkl_path):
-                    with open(pkl_path, 'rb') as file:
-                        graph_data = pickle.load(file)
-                        kps = self._transform_data(graph_data["keypoints"][:, :, :2])
-                        data_dict[vid_id] = {'label': labels[gloss], 'gloss': gloss, 'node_pos': kps, 'split': split}
+
+    def _load_pkl_files(self, pickle_path):
+        """ Load the pickle files and create a dictionary with the data
+        """
+        labels = {word: index for index, word in enumerate(self.gloss_dict.keys())}
+
+        data_dict = {
+            vid_id: {
+                'label': labels[gloss],
+                'gloss': gloss,
+                'node_pos': self._transform_data(pickle.load(open(os.path.join(pickle_path, f'{vid_id}.pkl'), 'rb'))["keypoints"][:, :, :2]),
+                'split': split
+            }
+            for gloss, metadata in self.gloss_dict.items()
+            for vid_id, split in metadata
+            if os.path.exists(os.path.join(pickle_path, f'{vid_id}.pkl'))
+        }
 
         return data_dict
+
     
-    ######################
+    #--------------------------------------
     # B. Pre-processing functionalities
-    ######################
+    #--------------------------------------
 
     def _transform_data(self, kps):
+        """ Apply selected transformations to the data
+        """
         frames = torch.tensor(np.asarray(kps, dtype=np.float32)).permute(2, 0, 1)
+        data = self._downsample_data(frames)
+
         # TODO: Introduce the other transformations
-        return self._downsample_data(frames)
+        
+        return data
 
     def _downsample_data(self, frames):
         """ Downsample pose graph based on the standard node selection from holistic 27 minimal nodes
         """
-        return frames[:, :, self.pose_indexes]
+        # Indexes for reduction of graph nodes of graph size 27 nodes, predefined in holistic mediapipe package 
+        pose_indexes = [0, 2, 5, 11, 12, 13, 14, 33, 37, 38, 41, 42, 45, 46, 49, 50, 53, 54, 58, 59, 62, 63, 66, 67, 70, 71, 74]
+        return frames[:, :, pose_indexes]
 
-    ######################
-    # C. Graph transformation functionalities
-    ######################
-    
-    def seperate_temporal_dimension(self, data_dict):
+    #--------------------------------------
+    # C. Graph construction functionalities
+    #--------------------------------------    
+    def _separate_temporal_dimension(self, data_dict):
+        """
+        Separates the temporal dimension of the data.
+        
+        For each video, it splits the data into individual frames and stores them 
+        with a unique key representing each frame of the video.
+        
+        :param data_dict: A dictionary with video data, keyed by video ID.
+        :return: A dictionary with data separated by individual frames.
+        """
         separated_frame_dict = {}
-    
+
+        node_features = torch.tensor(np.eye(self.N_NODES))  
+
         for vid_id, data in data_dict.items():
-            label = data['label']
-            gloss = data['gloss']
-            split = data['split']
+            label, gloss, split = data['label'], data['gloss'], data['split']
             node_pos = data['node_pos']
             num_frames = node_pos.shape[1]
+
             for frame_idx in range(num_frames):
                 new_key = f"{vid_id}_f_{frame_idx}"
                 frame_data = node_pos[:, frame_idx, :]
                 separated_frame_dict[new_key] = {
                     'label': label,
                     'gloss': gloss, 
+                    'x': node_features,  
                     'node_pos': frame_data,
                     'split': split
                 }
+
         return separated_frame_dict
     
-    def build_spatio_temporal_graph(self, data_dict):
-        ST_graph_dict = {}
-
-        
-        for vid_id, data in data_dict.items():
-            label = data['label']
-            gloss = data['gloss']
-            split = data['split']
-            node_pos = data['node_pos']
-            st_builder = SpatioTemporalGraphBuilder(node_pos, self.args)
-            reshaped_edges = st_builder.reshaped_edges
-            reshaped_data = st_builder.reshaped_data
-            ST_graph_dict[f"{vid_id}"] = {
-                    'label': label,
-                    'gloss': gloss, 
-                    'node_pos': reshaped_data,
-                    'edges': reshaped_edges,
-                    'split': split
-                }
-    
-        
-        return ST_graph_dict
-    
-    
-    def clean_padding(self, data_dict):
-        """ During pose extraction with mediapipe padding is added for reasons beyond me
-            This function removes all data points where all the node positions are zero
-            Yeah ok turns out it was very few points, but still
+    def _build_spatio_temporal_graph(self, data_dict):
         """
-        filtered_data_dict = {}
+        Builds a spatio-temporal graph from the provided data dictionary.
 
-        for key, data in data_dict.items():
-            # Check if all elements in node_pos are zeros
-            if not torch.all(data['node_pos'] == 0):
-                filtered_data_dict[key] = data
+        Each item in the data dictionary is transformed using the SpatioTemporalGraphBuilder,
+        and the resulting spatio-temporal graph data is stored in a new dictionary.
 
-        return filtered_data_dict       
+        :param data_dict: A dictionary containing video data.
+        :return: A dictionary containing the spatio-temporal graph data.
+        """
+        return {
+            vid_id: {
+                'label': data['label'],
+                'gloss': data['gloss'],
+                'x': st_builder.landmark_features,
+                'node_pos': st_builder.reshaped_data,
+                'edges': st_builder.reshaped_edges,
+                'split': data['split']
+            }
+            for vid_id, data in data_dict.items()
+            for st_builder in [SpatioTemporalGraphBuilder(data['node_pos'], self.args)]
+        }
+        
+  
     
 class SpatioTemporalGraphBuilder:
     def __init__(self, pos_data, args, inward_edges = None):
@@ -151,17 +160,25 @@ class SpatioTemporalGraphBuilder:
         :param num_nodes: Number of nodes in each frame.
         :param inward_edges: List of edges in the format [source, destination].
         """
+        print('Building graph...')
+        
         self.pos_data = pos_data
         self.args = args
+
         if args.reduce_graph:
-            self.pos_data = self.select_evenly_distributed_frames(self.pos_data, num_frames_to_select=self.args.n_frames)
+            self.pos_data = self._select_evenly_distributed_frames(self.pos_data, num_frames_to_select=self.args.n_frames)
+
         self.num_features = self.pos_data.shape[0]
         self.num_frames   = self.pos_data.shape[1]
-        self.num_nodes    = self.pos_data.shape[2]
+        self.N_NODES    = args.n_nodes
         
-        self.tot_number_nodes = self.num_frames * self.num_nodes
+        assert self.pos_data.shape[2] == self.N_NODES, "wrong configuration of number of nodes"
+
+        self.tot_number_nodes = self.num_frames * self.N_NODES
         
+        # Set inward edges    
         if inward_edges is None:
+            ## Default holistic mediapipe edges
             self.inward_edges = [[2, 0], [1, 0], [0, 3], [0, 4], [3, 5], [4, 6], [5, 7], [6, 17], 
                                 [7, 8], [7, 9], [9, 10], [7, 11], [11, 12], [7, 13], [13, 14], 
                                 [7, 15], [15, 16], [17, 18], [17, 19], [19, 20], [17, 21], [21, 22], 
@@ -169,10 +186,11 @@ class SpatioTemporalGraphBuilder:
         else:
             self.inward_edges = inward_edges
         
-        self.reshaped_edges = self.build_spatiotemporal_edges()
-        self.reshaped_data = self.reshape_nodes()
-        
-    def select_evenly_distributed_frames(self, tensor, num_frames_to_select=5):
+        self.reshaped_edges = self._build_spatiotemporal_edges()
+        self.reshaped_data = self._reshape_nodes()
+        self.landmark_features = self._build_node_features()
+
+    def _select_evenly_distributed_frames(self, tensor, num_frames_to_select=5):
         n_frames = tensor.shape[1]
         
         # Calculate indices 
@@ -183,11 +201,23 @@ class SpatioTemporalGraphBuilder:
 
         return selected_frames
             
-    def reshape_nodes(self):
+    def _reshape_nodes(self):
         return self.pos_data.reshape(self.pos_data.shape[0], -1)
     
+    def _build_node_features(self):
+        """
+        Builds the node features for a given number of frames.
+        :param num_frames: The number of frames in the data.
+        :return: A tensor with the node features in the graph.
+        """
+        identity_matrix = np.eye(self.N_NODES)
+        landmark_features = np.tile(identity_matrix, (1, self.num_frames))
+    
+        return torch.tensor(landmark_features)
 
-    def build_spatiotemporal_edges(self):
+
+
+    def _build_spatiotemporal_edges(self):
         """
         Builds the spatio-temporal edges for a given number of frames.
         :param num_frames: The number of frames in the data.
@@ -197,7 +227,7 @@ class SpatioTemporalGraphBuilder:
 
         # Adding spatial edges for each frame
         for frame in range(self.num_frames):
-            frame_offset = frame * self.num_nodes
+            frame_offset = frame * self.N_NODES
             for edge in self.inward_edges:
                 n1 = frame_offset + edge[0]
                 n2 = frame_offset + edge[1]
@@ -207,20 +237,23 @@ class SpatioTemporalGraphBuilder:
 
         # Adding temporal edges
         for frame in range(self.num_frames - 1):
-            for node in range(self.num_nodes):
-                n1 = frame * self.num_nodes + node
-                n2 = (frame + 1) * self.num_nodes + node
+            for node in range(self.N_NODES):
+                n1 = frame * self.N_NODES + node
+                n2 = (frame + 1) * self.N_NODES + node
                 edges.append([n1, n2])
                 
                 #edges.append([dst, src])  # Connecting the node to the next frame
 
         return torch.tensor(edges).t().contiguous()
+    
+    
 
 
 
 
 class PyGDataLoader:
     def __init__(self, data, args):
+        print('Building dataloader...')
         self.data_dict = data.data_dict
         self.batch_size = args.batch_size
         self.args = args
@@ -249,9 +282,10 @@ class PyGDataLoader:
         for id, data in data_dict.items():
             pos = data['node_pos'].T
             y = data['label']
+            x = data['x']
             if self.args.temporal_configuration == 'spatio_temporal':
                 self.edge_index = data['edges']
-            data_list.append(Data(pos = pos, x = pos ,edge_index= self.edge_index, y=y))
+            data_list.append(Data(pos = pos, x = x, edge_index= self.edge_index, y=y))
            
         
         print('Number of ' + split + ' points:', len(data_list))
@@ -275,7 +309,14 @@ if __name__ == "__main__":
                         help='Batch size. Does not scale with number of gpus.')
     parser.add_argument('--temporal_configuration', type=str, default="spatio_temporal",
                         help='Temporal configuration of the graph. Options: spatio_temporal, per_frame') 
-    
+        ## Graph size parameter
+    parser.add_argument('--reduce_graph', type=bool, default=False,
+                        help='Whether or not to reduce the graph to a limited number of frames') 
+    # TODO: Find a better way to set this number
+    parser.add_argument('--n_frames', type=float, default=10,
+                        help='Number of frames to use for the spatio temporal graph (max 12)') 
+    parser.add_argument('--n_nodes', type=int, default=27,
+                        help='Number of nodes to use when reducing the graph - only 27 currently implemented') 
     # Arg parser
     args = parser.parse_args()
 
