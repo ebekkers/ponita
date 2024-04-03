4ea8d11b8458bf0d973e14d733c759ae64b965e5
OlineRanum <oline.a.ranum@gmail.com>
2024-04-03 12:02:04 +0200

    added basecode for TCNUnit from MAP

--- ponita/models/temporal_ponita.py
+++ ponita/models/temporal_ponita.py
@@ -148,6 +148,81 @@ class TemporalPonita(PonitaFiberBundle):
         
         return x, graph
 
+def conv_init(conv):
+    nn.init.kaiming_normal_(conv.weight, mode="fan_out")
+    nn.init.constant_(conv.bias, 0)
+
+
+def bn_init(bn, scale):
+    nn.init.constant_(bn.weight, scale)
+    nn.init.constant_(bn.bias, 0)
+
+
+class TCNUnit(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=9,
+        stride=1,
+        use_drop=True,
+        drop_size=1.92,
+        num_points=25,
+        block_size=41,
+    ):
+        super(TCNUnit, self).__init__()
+        pad = int((kernel_size - 1) / 2)
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=(1, kernel_size),
+            padding=(0,pad),
+            stride=(1, stride),
+        )
+
+        self.bn = nn.BatchNorm2d(out_channels)
+        conv_init(self.conv)
+        bn_init(self.bn, 1)
+
+        self.use_drop = False
+        if use_drop:
+            self.dropT = DropGraphTemporal(block_size=block_size)
+
+    def forward(self, x, keep_prob=None, A=None):
+        # In dim, out dim, n_frames, nodes 
+        x = self.bn(self.conv(x))
+        if self.use_drop:
+            x = self.dropT(self.dropS(x, keep_prob, A), keep_prob)
+        return x
+
+
+class DropGraphTemporal(nn.Module):
+    def __init__(self, block_size=7):
+        super(DropGraphTemporal, self).__init__()
+        self.block_size = block_size
+
+    def forward(self, x, keep_prob):
+        self.keep_prob = keep_prob
+        if not self.training or self.keep_prob == 1:
+            return x
+
+        n, c, t, v = x.size()
+
+        input_abs = torch.mean(torch.mean(torch.abs(x), dim=3), dim=1).detach()
+        input_abs = (input_abs / torch.sum(input_abs) * input_abs.numel()).view(n, 1, t)
+        gamma = (1.0 - self.keep_prob) / self.block_size
+        input1 = x.permute(0, 1, 3, 2).contiguous().view(n, c * v, t)
+        M = torch.bernoulli(torch.clamp(input_abs * gamma, max=1.0)).repeat(1, c * v, 1)
+        m_sum = F.max_pool1d(
+            M, kernel_size=[self.block_size], stride=1, padding=self.block_size // 2
+        )
+        mask = (1 - m_sum).to(device=m_sum.device, dtype=m_sum.dtype)
+        return (
+            (input1 * mask * mask.numel() / mask.sum())
+            .view(n, c, v, t)
+            .permute(0, 1, 3, 2)
+        )
+
 
 if __name__ == "__main__":
     
